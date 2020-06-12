ca1fb896e4ffa6cbb0307f8b04263232573d4fe4
mattfasullo <matthewnf199@gmail.com>
2020-06-12 15:19:18 -0400

    Log in/Sign up screen, animated buttons
    Added new log in screen and sign up screen, animated some buttons, added log out button to settings.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject LogIn;
+    public GameObject SignUp;
     public GameObject MainMenu;
     public GameObject SessionsMenu;
     public GameObject Profile;
@@ -11,39 +13,78 @@ public class MainMenuManager : MonoBehaviour
     public GameObject Sessions;
     public GameObject Settings;
 
+    public GameObject SettingsButton;
+
     public void SessionMenu() // reveals Session Menu
     {
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
         MainMenu.SetActive(false);
         Exposure.SetActive(false);
         Profile.SetActive(false);
         Sessions.SetActive(false);
         SessionsMenu.SetActive(true);
+        SettingsButton.SetActive(true);
     }
     public void ProfileMenu() // reveals Profile Menu
     {
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
         MainMenu.SetActive(false);
         Exposure.SetActive(false);
         Profile.SetActive(true);
         Sessions.SetActive(false);
         SessionsMenu.SetActive(false);
+        SettingsButton.SetActive(true);
     }
     public void ExposureMenu() // reveals Exposure Menu
     {
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
         MainMenu.SetActive(false);
         Exposure.SetActive(true);
         Profile.SetActive(false);
         Sessions.SetActive(false);
         SessionsMenu.SetActive(false);
+        SettingsButton.SetActive(true);
     }
 
     public void Menu()  // reveals Main Menu
     {
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
         MainMenu.SetActive(true);
         Exposure.SetActive(false);
         Profile.SetActive(false);
         Sessions.SetActive(false);
         SessionsMenu.SetActive(false);
         Settings.SetActive(false);
+        SettingsButton.SetActive(true);
+    }
+
+    public void LogInMenu()  // reveals Log In Menu
+    {
+        LogIn.SetActive(true);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void SignUpMenu()  // reveals Sign Up Menu
+    {
+        LogIn.SetActive(false);
+        SignUp.SetActive(true);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
     }
 
 }
---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public string stringToEdit = "HelloWorld!";
+
+    private void OnGUI()
+    {
+        stringToEdit = GUILayout.TextField(stringToEdit, 25);
+    }
+}
