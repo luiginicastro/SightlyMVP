2a476b8027128f1e752d977ac8cd4715b5fcf9d2
mattfasullo <matthewnf199@gmail.com>
2020-06-19 12:37:09 -0400

    Added sessions sub-menus
    Now we can access the "levels" of each session, and the levels that have videos can be accessed.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Main Menus")]
     public GameObject LogIn;
     public GameObject SignUp;
     public GameObject MainMenu;
@@ -13,6 +14,16 @@ public class MainMenuManager : MonoBehaviour
     public GameObject Sessions;
     public GameObject Settings;
 
+    [Header("Session Sub-menus")]
+    public GameObject interviews;
+    public GameObject publicSpeaking;
+    public GameObject socialGatherings;
+    public GameObject criticism;
+    public GameObject introductions;
+    public GameObject beingTeased;
+    public GameObject session7;
+
+    [Header("Other")]
     public GameObject SettingsButton;
 
     public void SessionMenu() // reveals Session Menu
@@ -25,6 +36,14 @@ public class MainMenuManager : MonoBehaviour
         Sessions.SetActive(false);
         SessionsMenu.SetActive(true);
         SettingsButton.SetActive(true);
+
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
     }
     public void ProfileMenu() // reveals Profile Menu
     {
@@ -36,6 +55,14 @@ public class MainMenuManager : MonoBehaviour
         Sessions.SetActive(false);
         SessionsMenu.SetActive(false);
         SettingsButton.SetActive(true);
+
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
     }
     public void ExposureMenu() // reveals Exposure Menu
     {
@@ -47,8 +74,15 @@ public class MainMenuManager : MonoBehaviour
         Sessions.SetActive(false);
         SessionsMenu.SetActive(false);
         SettingsButton.SetActive(true);
-    }
 
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+    }
     public void Menu()  // reveals Main Menu
     {
         LogIn.SetActive(false);
@@ -60,8 +94,15 @@ public class MainMenuManager : MonoBehaviour
         SessionsMenu.SetActive(false);
         Settings.SetActive(false);
         SettingsButton.SetActive(true);
-    }
 
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+    }
     public void LogInMenu()  // reveals Log In Menu
     {
         LogIn.SetActive(true);
@@ -73,6 +114,14 @@ public class MainMenuManager : MonoBehaviour
         SessionsMenu.SetActive(false);
         Settings.SetActive(false);
         SettingsButton.SetActive(false);
+
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
     }
     public void SignUpMenu()  // reveals Sign Up Menu
     {
@@ -85,6 +134,156 @@ public class MainMenuManager : MonoBehaviour
         SessionsMenu.SetActive(false);
         Settings.SetActive(false);
         SettingsButton.SetActive(false);
+
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
     }
 
+
+
+    public void InterviewsLevels()  // reveals...
+    {
+        interviews.SetActive(true);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void PublicSpeakingLevels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(true);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void SocialGatheringsLevels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(true);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void CriticismLevels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(true);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void IntroductionsLevels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(true);
+        beingTeased.SetActive(false);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void BeingTeasedLevels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(true);
+        session7.SetActive(false);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
+    public void Session7Levels()  // reveals...
+    {
+        interviews.SetActive(false);
+        publicSpeaking.SetActive(false);
+        socialGatherings.SetActive(false);
+        criticism.SetActive(false);
+        introductions.SetActive(false);
+        beingTeased.SetActive(false);
+        session7.SetActive(true);
+
+        LogIn.SetActive(false);
+        SignUp.SetActive(false);
+        MainMenu.SetActive(false);
+        Exposure.SetActive(false);
+        Profile.SetActive(false);
+        Sessions.SetActive(false);
+        SessionsMenu.SetActive(false);
+        Settings.SetActive(false);
+        SettingsButton.SetActive(false);
+    }
 }
