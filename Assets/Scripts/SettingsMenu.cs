672ad95f0b045beb2c27d46364fd77389d7ef364
mattfasullo <matthewnf199@gmail.com>
2020-06-19 10:57:23 -0400

    Revert "started change for new version"
    This reverts commit 8c50a1e3974dfb01f1c4a65c9cd2387821c90b38.

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -5,14 +5,29 @@ using UnityEngine.Video;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
+    public GameObject settingsMenu; // this is the settingsmenu object
+    public GameObject settingsToggleOn;
+    public GameObject settingsToggleOff;
     public VideoPlayer vp;
 
 
-    public void PauseMenu()
+
+    public void SettingsOn()
     {
-        pauseMenu.SetActive(true);
+        settingsMenu.SetActive(true); //this turns on the settings panel
+        settingsToggleOn.SetActive(false);
+        settingsToggleOff.SetActive(true);
         vp.Pause();
+        vp.GetComponent<MeshRenderer>().enabled = false;
+
     }
-    
+
+    public void SettingsOff()
+    {
+        settingsMenu.SetActive(false); //this turns off the settings panel
+        settingsToggleOn.SetActive(true);
+        settingsToggleOff.SetActive(false);
+        vp.Play();
+        vp.GetComponent<MeshRenderer>().enabled = true;
+    }
 }
